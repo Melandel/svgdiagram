0017891e3e1ac6cfcb5088458837c406c76c9631
Melandel <48007894+Melandel@users.noreply.github.com>
2019-08-17 01:17:31 +0200

    Handle SVG initialization from DOM element

--- src/drawDiagram.js
+++ src/drawDiagram.js
@@ -1,5 +1,11 @@
 let drawDiagram = function drawDiagram (domContainerID, title, drawContent) {
-	let svg = SVG(domContainerID);
+	let svg = null;
+	if (typeof domContainerID === "string")
+		svg = SVG(domContainerID);
+	else {
+		domContainerID.id = domContainerID.id || ("svgContainer" + Math.floor(Math.random() * 1001));
+		svg = SVG(domContainerID.id);
+	}
 	svg.node.addEventListener("mousedown", function (ev) { if (ev.button === 2) { this.style.cursor = "move";    } });
 	svg.node.addEventListener("mouseup",   function (ev) { if (ev.button === 2) { this.style.cursor = "default"; } });
 
@@ -7,28 +13,6 @@ let drawDiagram = function drawDiagram (domContainerID, title, drawContent) {
 	window.isFirstNode = true;
 	
 	let drawingTool = function(...args) {
-		/* NODES // depth:4
-			drawingTool("title")
-			drawingTool("title", "content")
-			drawingTool("title", ["cell1", "cell2"])
-			drawingTool("title", ["cellA1", "cellA2"], ["cellB1", "cellB2"])
-		   FRAMES // depth:3
-			drawingTool("title", node)
-			drawingTool("title", node1, node2)
-			drawingTool("title", node1, node2, text)
-			drawingTool("title", node1, node2, text1, text2)
-		   SETS // depth:2
-			drawingTool("title", frame)
-			drawingTool("title", frame1, frame2)
-			drawingTool("title", frame1, frame2, node1, node2, text1, text2)
-		   BIGSETS // depth:1
-			drawingTool("title", set)
-			drawingTool("title", set1, set2)
-			drawingTool("title", set1, set2, frame1, frame2, node1, node2, text1, text2)
-		   BACKGROUND // depth:0
-		*/
-		
-		// Dummy implementation
 		let drawn;
 		if(args.some(x => x instanceof SVG.Frame || x instanceof SVG.RNode))
 			drawn = svg.frame(...args);
